34d065e502cb84da7bd0c863009500b6fb9555ab
JayaShree-S-BizDom <jayashree.bizdom@gmail.com>
2024-09-25 12:16:06 +0530

    Second Push

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,3 +3,11 @@ node -v
 
 echo "Checking npm version..."
 npm -v
+
+# Set JAVA_HOME to point to Java 11
+export JAVA_HOME=/path/to/your/java11
+
+echo "JAVA_HOME set to $JAVA_HOME"
+
+# Check the Java version
+java -version
